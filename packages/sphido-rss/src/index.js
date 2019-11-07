23a41b316e7eb3b80e8950ba69e6d74f66e5d1d3
Roman Ožana <roman@omdesign.cz>
2019-11-07 15:31:43 +0100

    Fix function name in RSS module

--- packages/sphido-rss/src/index.js
+++ packages/sphido-rss/src/index.js
@@ -32,7 +32,7 @@ const toAtomItem = function (post) {
  * @param options
  * @returns {string}
  */
-module.exports = function rss(posts, options, atomLink) {
+module.exports = function (posts, options, atomLink) {
 	return `<?xml version="1.0" encoding="UTF-8"?>` +
 		`<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/"><channel>` +
 		(atomLink ? `<atom:link href="${atomLink}" rel="self" type="application/rss+xml" />` : '') +
