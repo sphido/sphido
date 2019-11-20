5f749903dde88830b2850f47c9cb82edcdf8b822
Roman Ožana <roman@omdesign.cz>
2019-11-20 10:24:11 +0100

    Delete unnecessary test

--- packages/sphido-nunjucks/test/nunjucks.js
+++ packages/sphido-nunjucks/test/nunjucks.js
@@ -6,13 +6,6 @@ test('example content', async t => {
 	t.is(result, 'example content');
 });
 
-/*
-test('strip H1', async t => {
-	const result = await nunjucks.renderString('{{content|h1strip}}', {content: '<h1>strip h1</h1>'});
-	t.is(result, '');
-});
-*/
-
 test('custom filter', async t => {
 	env.addFilter('xxx', str => 'working well');
 	const result = await renderString('{{content|xxx}}', {content: '<p>strip h1</p>'});
