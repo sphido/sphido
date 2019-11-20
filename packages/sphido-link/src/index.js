e56094ff8afcd540890a46d4fa18c8c751c1026b
Roman Ožana <roman@omdesign.cz>
2019-11-20 12:18:13 +0100

    Domain in @sphido/link

--- packages/sphido-link/src/index.js
+++ packages/sphido-link/src/index.js
@@ -7,8 +7,8 @@ module.exports = {
 	 * Generate link to current page
 	 * @returns {string}
 	 */
-	link() {
-		return '/' + normalize(this.dir + sep + this.slug + sep)
+	link(domain = '/') {
+		return domain + normalize(this.dir + sep + this.slug + sep)
 			.split(sep)
 			.splice(1)
 			.join('/');
