abad0793dd431f49948f3104564851e0ab34b9ab
Roman Ožana <roman@omdesign.cz>
2019-11-20 12:31:44 +0100

    Fix sitemap link function

--- packages/sphido-sitemap/src/index.js
+++ packages/sphido-sitemap/src/index.js
@@ -7,6 +7,6 @@
 module.exports = (pages, domain) => {
 	return '<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n' +
 		`<url><loc>${domain}</loc><lastmod>${new Date().toISOString()}</lastmod><priority>1.0</priority></url>\n` +
-		pages.map(post => `<url><loc>${post.link || post.link(domain)}</loc><lastmod>${post.date.toISOString()}</lastmod><priority>0.80</priority></url>\n`).join('') +
+		pages.map(post => `<url><loc>${typeof post.link === "function" ? post.link(domain) : post.link}</loc><lastmod>${post.date.toISOString()}</lastmod><priority>0.80</priority></url>\n`).join('') +
 		'</urlset>';
 };
--- packages/sphido-sitemap/test/sitemap.js
+++ packages/sphido-sitemap/test/sitemap.js
@@ -10,6 +10,11 @@ const posts = [
 	{
 		link: 'https://sitemap.com/example2',
 		date: new Date()
+	},
+
+	{
+		link: () => 'generated with link function',
+		date: new Date()
 	}
 ];
 
@@ -18,4 +23,5 @@ test('sitemap.xml basics', t => {
 	t.is(s.includes('<?xml version="1.0" encoding="UTF-8"?>'), true);
 	t.is(s.includes('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'), true);
 	t.is(s.includes('<loc>https://www.site.com</loc>'), true);
+	t.is(s.includes('<loc>generated with link function</loc>'), true);
 });
