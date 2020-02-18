d5933e71e507bac703e247a5d2d4389601a9ad3d
Roman Ožana <roman@omdesign.cz>
2020-02-18 09:13:37 +0100

    Deps upgrade

--- packages/sphido-core/test/get-page.js
+++ packages/sphido-core/test/get-page.js
@@ -1,7 +1,6 @@
 import {join} from 'path';
 import test from 'ava';
-
-const {getPage} = require('..');
+import {getPage} from '..';
 
 test('RSS basics', async t => {
 	const page = await getPage(join(__dirname, '/page.html'));
