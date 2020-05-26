dd9dd0ea24bd4b4f5eee14f4364d7223c5ea08b7
Roman Ožana <roman@omdesign.cz>
2020-05-26 09:26:23 +0200

    Fix typos

--- packages/sphido-feed/src/index.js
+++ packages/sphido-feed/src/index.js
@@ -21,26 +21,24 @@ module.exports = function (posts, options, feed = undefined) {
 			return '';
 		}
 
-		return isNaN(Number(tag)) ? `<${tag}>${value}</${tag}>` : value;
+		return Number.isNaN(Number(tag)) ? `<${tag}>${value}</${tag}>` : value;
 	}).join('');
-
-	const toAtomItem = function (post) {
-		return {
-			item: {
-				guid: typeof post.link === 'function' ? post.link() : post.link,
-				title: post.title ? post.title : undefined,
-				link: typeof post.link === 'function' ? post.link() : post.link,
-				pubDate: post.date instanceof Date ? post.date.toUTCString() : undefined,
-				description: post.description ? `<![CDATA[${post.description}]]>` : undefined,
-				'content:encoded': post.content ? `<![CDATA[${post.content.replace(/<h1.*>.*?<\/h1>/g, '')}]]>` : undefined
-			}
-		};
-	};
 
 	return '<?xml version="1.0" encoding="UTF-8"?>' +
 		'<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom" xmlns:content="http://purl.org/rss/1.0/modules/content/"><channel>' +
 		(feed ? `<atom:link href="${feed}" rel="self" type="application/rss+xml" />` : '') +
 		toXML(Object.assign({}, defaultOptions, options)) +
-		toXML(posts.map(toAtomItem)) +
+		toXML(posts.map(post => {
+			return {
+				item: {
+					guid: typeof post.link === 'function' ? post.link() : post.link,
+					title: post.title ? post.title : undefined,
+					link: typeof post.link === 'function' ? post.link() : post.link,
+					pubDate: post.date instanceof Date ? post.date.toUTCString() : undefined,
+					description: post.description ? `<![CDATA[${post.description}]]>` : undefined,
+					'content:encoded': post.content ? `<![CDATA[${post.content.replace(/<h1.*>.*?<\/h1>/g, '')}]]>` : undefined
+				}
+			};
+		})) +
 		'</channel></rss>';
 };
