a321cf51d78c7424ca1b69360378bb781a9959b5
Roman Ožana <roman@omdesign.cz>
2020-09-14 11:18:45 +0200

    Fixing tests :facepalm:

--- packages/sphido-markdown/test/markdown.js
+++ packages/sphido-markdown/test/markdown.js
@@ -1,21 +1,5 @@
 import test from 'ava';
-import {parse as markdown, renderer} from '..';
-
-test('basics renderer H1 test', t => {
-	const page = {content: '# H1'};
-
-	// @see https://marked.js.org/using_pro#renderer
-
-	renderer({
-		heading: (text, level) => {
-			return `<h1>XXX - ${text.toLowerCase()} - XXX</h1>`
-		}
-	});
-
-	markdown(page);
-
-	t.is(page.content, '<h1>XXX - h1 - XXX</h1>');
-});
+import {markdown, renderer} from '..';
 
 test('basics renderer H1 test', t => {
 	const page = {content: '# H1'};
