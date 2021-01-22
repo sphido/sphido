681f59403b714c9e545d2ef25ce90d5c068d97d6
Roman Ožana <roman@omdesign.cz>
2021-01-22 12:43:11 +0100

    Fix default export

--- packages/sphido-emoji/lib/emoji.js
+++ packages/sphido-emoji/lib/emoji.js
@@ -4,6 +4,6 @@ import twemoji from 'twemoji';
  * Process page content and replace with emoji
  * @param {Object} page
  */
-export default function emoji(page) {
+export function emoji(page) {
 	page.content = twemoji.parse(page.content); // Emoji
 }
