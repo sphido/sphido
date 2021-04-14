ca35eccfb0898b4bae55c366c6984e7358d3a317
Roman Ožana <roman@ozana.cz>
2021-04-14 14:42:45 +0200

    Fix pages range in pagination

--- packages/sphido-pagination/lib/pagination.js
+++ packages/sphido-pagination/lib/pagination.js
@@ -1,18 +1,29 @@
 /**
+ * Generate pages range
+ * @param {int} total
+ */
+function * pages(total) {
+	for (let i = 1; i <= total; i++) {
+		yield i;
+	}
+}
+
+/**
  * Generate pagination
  * @returns {AsyncIterableIterator<*>}
  * @param {array} posts
  * @param {int} perPage
  */
 export async function * pagination(posts, perPage = 5) {
-	const pages = Math.ceil(posts.length / perPage);
+	const total = Math.ceil(posts.length / perPage);
 	let current = 1;
 
-	while (current <= pages) {
+	while (current <= total) {
 		yield {
+			total,
 			posts: posts.slice(perPage * (current - 1), current * perPage),
 			current,
-			pages
+			pages: [...pages(total)]
 		};
 		current++;
 	}
