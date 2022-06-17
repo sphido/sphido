1488967618574825ebd99b143287d1e48099b5ac
Roman Ožana <roman@ozana.cz>
2022-06-17 16:45:24 +0200

    @sphido/emoji is deprecated

--- packages/sphido-emoji/lib/emoji.js
+++
@@ -1,9 +0,0 @@
-import twemoji from 'twemoji';
-
-/**
- * Process page content and replace with emoji
- * @param {Object} page
- */
-export function emoji(page) {
-	page.content = twemoji.parse(page.content); // Emoji
-}
