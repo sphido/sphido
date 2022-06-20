d398b6d985b79874a00bf6fdb536d2c5f6826b1b
Roman Ožana <roman@ozana.cz>
2022-06-20 22:26:06 +0200

    Fix npm package

--- packages/sphido-sitemap/examples/index.js
+++ packages/sphido-sitemap/examples/index.js
@@ -3,7 +3,7 @@
 import {dirname, relative, join} from 'node:path';
 import {getPages, allPages} from '@sphido/core';
 import slugify from '@sindresorhus/slugify';
-import {createSitemap} from '../lib/sitemap.js';
+import {createSitemap} from '@sphido/sitemap';
 
 const pages = await getPages({path: 'content'});
 const map = await createSitemap('sitemap.xml');
