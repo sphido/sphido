da7e1edc3540039bbf8eca42b13cc3a61cfbaf14
Roman Ožana <roman@ozana.cz>
2022-07-01 14:10:42 +0200

    Fix .html

--- packages/sphido-hashtags/lib/tags-to-markdown.js
+++ packages/sphido-hashtags/lib/tags-to-markdown.js
@@ -1,9 +1,5 @@
 import slugify from '@sindresorhus/slugify';
 
-function defaultTagToUrl(tag) {
-	return `${slugify(tag)}.html`;
-}
-
 /**
  * Convert #hashtags to Markdown links
  *
@@ -13,7 +9,7 @@ function defaultTagToUrl(tag) {
  * @param {Function} tagToUrl
  * @returns {string}
  */
-export function tagsToMarkdown(content, tags = [], {urlBase = '/tag/', tagToUrl = defaultTagToUrl} = {}) {
+export function tagsToMarkdown(content, tags = [], {urlBase = '/tag/', tagToUrl = slugify} = {}) {
 	if (tags && tags.length > 0) {
 		const anchor = new RegExp('(' + tags.join('|') + ')', 'gmi');
 		return content.replace(anchor, (match, capture) => `[${match}](${urlBase}${tagToUrl(capture)})`);
